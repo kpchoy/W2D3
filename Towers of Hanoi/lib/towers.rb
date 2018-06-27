bb6786c2ff52f509bb4329ffb662543119e8e8e1
App Academy Student <appacademy@Apps-Mac-mini-4.local>
2018-06-27 15:39:26 -0700

    finished towers of hanoi

---
+++ Towers of Hanoi/lib/towers.rb
@@ -0,0 +1,61 @@
+class Towers
+  attr_accessor :piles  
+  def initialize
+    @piles = [[3,2,1],[],[]]
+  end
+  
+  def display
+    @piles.each do |el|
+      p el
+    end
+  end
+  
+  def move_pile(array)
+    moving_disk = @piles[array[0]].pop 
+    @piles[array[1]].push(moving_disk)
+  end
+  
+  def valid_move?(array)
+    if @piles[array[0]].nil? || @piles[array[1]].nil?
+      return false
+    elsif @piles[array[0]].empty? || @piles[array[1]].last == 1
+      return false 
+    elsif array[0].between?(0,2) && array[1].between?(0,2)
+      return true
+    end 
+    false
+  end
+  
+  def gets_move
+    move = []
+    puts "Which pile would you like to move from"
+    move << gets.chomp.to_i 
+    puts "Which pile would you like to move to"
+    move << gets.chomp.to_i 
+    move
+  end
+  
+  def won?
+    return false unless @piles[1] == [3,2,1] || @piles[2] == [3,2,1]
+    true 
+  end
+  
+  def play 
+    until won?
+      display 
+      pos = gets_move
+      if valid_move?(pos) == false 
+        puts "Please pick again"
+      else valid_move?(pos)
+        move_pile(pos)
+      end
+    end 
+    p "Game over, you won!"
+  end 
+end
+
+if $PROGRAM_NAME == __FILE__ 
+  game = Towers.new
+  game.play 
+end 
+  
---
+++ Towers of Hanoi/spec/towers_spec.rb
@@ -0,0 +1,54 @@
+require "towers"
+require "rspec"
+
+
+RSpec.describe "Towers" do 
+  subject(:towers) {Towers.new}
+
+  describe "#display" do
+    it "piles start with three arrays" do
+      expect(towers.display).to include([],[],[3,2,1]) 
+    end
+  end
+  
+  describe "#move_pile" do 
+    it "correctly moves the disk" do
+      towers.move_pile([0,1])
+      towers.move_pile([0,2])
+      expect(towers.piles).to eq([[3],[1],[2]])
+    end
+  end 
+  
+  describe "#valid_move?" do
+    it "return true if move is valid" do
+      expect(towers.valid_move?([0,1])).to be true
+    end
+    
+    it "return false if move is invalid" do
+      expect(towers.valid_move?([1,3])).to be false
+    end
+  end
+  
+  describe "#won?" do 
+    it "returns false when game is initialized" do
+      expect(towers.won?).to be false 
+    end
+
+  let(:towers2) {Towers.new}
+    it "returns false when tower is not stacked in order" do
+      towers2.piles = [[], [2, 3, 1], []]
+      expect(towers2.won?).to be false 
+    end
+      
+    it "returns true when tower is stacked correctly" do
+      towers2.piles = [[], [3, 2, 1], []]
+      expect(towers2.won?).to be true 
+    end
+  
+  end 
+  
+end
+
+
+
+
